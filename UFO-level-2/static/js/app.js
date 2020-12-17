9ddee003059d96d42a8ef47fd651862c8803a028
Tim Samson <9625746+timsamson@users.noreply.github.com>
2020-12-17 14:33:03 -0500

    changed folder layout

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,54 @@
+// from data.js
+var tableData = data;
+
+// Select table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+//Create row for each data set
+    var row = tbody.append("tr");
+
+//Insert Cell Values
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+
+  //Select button
+  var button = d3.select("#filter-btn");
+    button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input date get the raw HTML nodes
+    var input = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = input.property("value");
+    console.log(inputValue);
+
+    // Filter Data
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    // console.log filter values
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+
+        console.log(selections);
+        // Append one table row `tr` for each sighting
+        var row = tbody.append("tr");
+
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    });
+
+
