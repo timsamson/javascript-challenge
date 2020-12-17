92053fb4153161c83ea3d93be989a8e465891f52
Tim Samson <9625746+timsamson@users.noreply.github.com>
2020-12-17 15:06:55 -0500

    serach fucntion

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -27,5 +27,91 @@ var loadTableRows = (whichData) => {
 loadTableRows(tableData);
 
 //
+// Search button
+btnSearch.on("click", () => {
+	d3.event.preventDefault();
 
+    var searchedDate = searchDate.property("value");
+    var searchedCity = searchCity.property("value");
+    var searchedState = searchState.property("value");
+    var searchedCountry = searchCountry.property("value");
+    var searchedShape = searchShape.property("value");
+		
+//Date
+	if(searchedDate){
+		var tableData_Filtered = tableData.filter(tableData => tableData.datetime === searchedDate);
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear HTML & add user message
+			tbody.html("");	
+			tbody.append("tr").append("td").text("Sorry no sightings listed");
+		}
+	}
+//City
+	else if(searchedCity) {
+		var tableData_Filtered = tableData.filter(tableData => tableData.city === searchedCity);
+	
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear HTML & add user message
+			tbody.html("");
+			tbody.append("tr").append("td").text("Sorry no sightings listed");
+		}
+	}
+//State
+	else if(searchedState) {
+		var tableData_Filtered = tableData.filter(tableData => tableData.state === searchedState);
+	
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear HTML & add user message
+			tbody.html("");
+			tbody.append("tr").append("td").text("Sorry no sightings listed");
+		}
+	}
+//Country
+	else if(searchedCountry) {
+		var tableData_Filtered = tableData.filter(tableData => tableData.country === searchedCountry);
+	
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear HTML & add user message
+			tbody.html("");
+			tbody.append("tr").append("td").text("Sorry no sightings listed");
+		}
+	}
+//Shape
+	else { 
+		var tableData_Filtered = tableData.filter(tableData => tableData.shape === searchedShape);
+	
+		if(tableData_Filtered.length !== 0) {
+			loadTableRows(tableData_Filtered);
+		}
+		else {
+			// Clear HTML & add user message
+			tbody.html("");
+			tbody.append("tr").append("td").text("Sorry no sightings listed");
+		}
+	}
+})
+
+// Reset
+btnReset.on("click", () => {
+	document.getElementById("searchDate").value='';
+	document.getElementById("searchCity").value='';
+	document.getElementById("searchState").value='';
+	document.getElementById("searchCountry").value='';
+	document.getElementById("searchShape").value='';
+	
+// Load full dataset
+	loadTableRows(tableData);
+})
 
