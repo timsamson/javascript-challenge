498b46a12145d41c5ac5aff1d8598d8d36b290fc
Tim Samson <9625746+timsamson@users.noreply.github.com>
2020-12-18 19:58:56 -0500

    code cleanup

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,32 +1,27 @@
 // from data.js
 var tableData = data;
 var tblColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
 // Refereces
 var tbody = d3.select("tbody");
+
 var btnSearch = d3.select("#btnSearch");
 var btnReset = d3.select("#btnReset");
-
 var searchDate = d3.select("#searchDate");
 var searchCity = d3.select("#searchCity");
 var searchState = d3.select("#searchState");
 var searchCountry = d3.select("#searchCountry");
 var searchShape = d3.select("#searchShape");
 
-//insert serach function here
-
 var loadTableRows = (whichData) => { 
-
     tbody.html("");
-	
 	whichData.forEach(dataRow => { 
 		var tblRow = tbody.append("tr");  
 		tblColumns.forEach(column => tblRow.append("td").text(dataRow[column]))
 	});
 }
-
 loadTableRows(tableData);
 
-//
 // Search button
 btnSearch.on("click", () => {
 	d3.event.preventDefault();
@@ -103,7 +98,7 @@ btnSearch.on("click", () => {
 	}
 })
 
-// Reset
+// Reset form to blank
 btnReset.on("click", () => {
 	document.getElementById("searchDate").value='';
 	document.getElementById("searchCity").value='';
